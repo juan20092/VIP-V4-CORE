2b52aa6a994bb129f43e1ec636e25956d4075037
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-04-27 18:17:22 -0500

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -7,8 +7,8 @@ const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 
 var handler = m => m
 handler.all = async function (m) { 
-global.canalIdM = ["120363422142340004@newsletter", "120363422142340004@newsletter"]
-global.canalNombreM = ["☃️ 𝑲𝒂𝒏𝒆𝒌𝒊𝑩𝒐𝒕 - 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 🌿", "🔥 𝐊𝐚𝐧𝐞𝐊𝐢𝐁𝐨𝐭-𝐕3 - 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 ᯓ🍋"]
+global.canalIdM = ["120363419404216418@newsletter", "120363419404216418@newsletter"]
+global.canalNombreM = ["♑︎ 𝐌𝐄𝐋𝐈𝐎𝐃𝐀𝐒 - 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 ꨄ", "♑︎ 𝐌𝐄𝐋𝐈𝐎𝐃𝐀𝐒 - 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 ꨄ"]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
@@ -19,21 +19,19 @@ global.mes = d.toLocaleDateString('es', {month: 'long'})
 global.año = d.toLocaleDateString('es', {year: 'numeric'})
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
-var canal = 'https://whatsapp.com/channel/0029VbC34Nt42DchIWA0q11f'  
-var comunidad = 'https://whatsapp.com/channel/0029VbC34Nt42DchIWA0q11f'
+var canal = 'https://whatsapp.com/channel/0029VbARAwbEgGfOsCN1T10V'  
 var git = 'https://github.com/Shadow-nex'
 var github = 'https://github.com/Shadow-nex/KanekiBot-V3' 
-var correo = 'shadowcore.xyz@gmail.com'
-global.redes = [canal, comunidad, git, github, correo].getRandom()
+var correo = 'juanboli2009@gmail.com'
+global.redes = [canal,git, github, correo].getRandom()
 
 global.nombre = m.pushName || 'Anónimo'
-global.packsticker = `°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n🌿 Usuario: ${nombre}\n🎀 Bot: ${botname}\n📆 Fecha: ${fecha}\nⴵ Hora: ${moment.tz('America/Caracas').format('HH:mm:ss')}`
+global.packsticker = `°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n🌿 Usuario: ${nombre}\n🔮 Bot: ${botname}\n📆 Fecha: ${fecha}\nⴵ Hora: ${moment.tz('America/Caracas').format('HH:mm:ss')}`
 global.packsticker2 = `\n°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n\n🍃 ${dev}`
 
 global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
 }}, { quoted: m }
 
-// (Horario Perú 🇵🇪)
 var ase = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Lima" }))
 var hour = ase.getHours()
 switch (true) {
@@ -63,12 +61,10 @@ global.nombre = m.pushName || 'Anónimo'
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 global.icono = [
-'https://i.pinimg.com/originals/b3/67/d5/b367d513d861de468305c32c6cd22756.jpg', 
-'https://cdn.yupra.my.id/yp/mmuj995h.jpg', 
-'https://cdn.yupra.my.id/yp/ey5l5cct.jpg',
-'https://cdn.yupra.my.id/yp/o720p39m.jpg',
-'https://cdn.yupra.my.id/yp/a81y4vy5.jpg',
-'https://cdn.yupra.my.id/yp/uwx1azzu.jpg'
+'https://api.dix.lat/media2/1777327424999.jpg', 
+'https://api.dix.lat/media2/1777327498020.jpg', 
+'https://api.dix.lat/media2/1777331725085.jpg',
+'https://api.dix.lat/media2/1777331743803.jpg'
 ].getRandom()
 /*
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
@@ -83,7 +79,7 @@ global.rcanal = {
       newsletterName: channelRD.name,
     },
     externalAdReply: {
-      title: `•◍⃝߭۫ꯨ🍨۪〬.࠭⤿ʅ ${botname} 𑁍ꥈ࣪⬪🍃`,
+      title: `•◍⃝߭۫ꯨꨄ🌟.࠭⤿ʅ ${botname} 𑁍ꥈ࣪⬪🍃`,
       body: `（つ /${nombre} • ${saludo}🍓꒱`,
       mediaUrl: null,
       description: null,
@@ -104,7 +100,7 @@ global.rcanalx = {
       newsletterName: channelRD.name,
     },
     externalAdReply: {
-      title: '❒︴🍃 🄰🄲🄲🄴🅂🄾 • 🄳🄴🄽🄴🄶🄰🄳🄾 🍬✗',
+      title: '❒︴🍃 🄰🄲🄲🄴🅂🄾 • 🄳🄴🄽🄴🄶🄰🄳🄾 🔥✗',
       body: `（๑•ᴗ• )づ ${dev}`,
       mediaUrl: null,
       description: null,
